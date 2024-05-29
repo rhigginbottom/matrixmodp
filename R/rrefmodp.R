7fec460d158dc425b9c15321188d4a2e94bedef9
Ryan Higginbottom <ryan.higginbottom@gmail.com>
2024-05-29 16:00:14 -0400

    Removed extraneous comments

--- R/rrefmodp.R
+++ R/rrefmodp.R
@@ -46,37 +46,28 @@ rref_p <- function(A, p) {
   i <- j <- 1
   while (i <= m && j <= n) {
     while (j <= n) {
-      # w <- c(i,j) # used in debugging
-      # print(w)
       col <- A[, j]
-      col[1:m < i] <- 0 # put 0 in entries above current row
-      # find maximum pivot in current column at or below current row
+      col[1:m < i] <- 0
       which <- which.max(col)
       pivot <- A[which, j]
-      # check for 0 pivot
       if (which < i | pivot == 0) {
         j <- j + 1
         next
       }
-      # move pivot if needed
       if (which > i) A[c(i, which), ] <- A[c(which, i), ]
-      # find multiplicative inverse of pivot
       for (k in 1:p) {
         piv_inv <- k
         if (((pivot * k) %% p) == 1) {
           break
         }
       }
-      # multiply row by multiplicative inverse of pivot
       A[i, ] <- A[i, ] * piv_inv
       row <- A[i, ] %% p
-      # clear out all entries in column except pivot
       A <- A - outer(A[, j], row)
       A[i, ] <- row
       A <- A %% p
       j <- j + 1
       break
-      # i <- i+1
     }
     i <- i + 1
   }
